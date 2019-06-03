09242d1bd6f10f310aeada663d859682da99e6bc
rewit <rewit@naver.com>
2019-06-03 16:34:34 +0900

    Java_04

---
+++ src/com/biz/if_then/If_06.java
@@ -0,0 +1,20 @@
+package com.biz.if_then;
+
+public class If_06 {
+
+	public static void main(String[] args) {
+
+		int intNum = 21;
+		int index = 2;
+		for(index = 2 ; index < intNum ; index++) {
+			if( intNum % index == 0) { 
+				System.out.println(intNum + "는 소수가 아닙니다");
+				break;
+				}
+
+	}if (index >= intNum) {
+		System.out.println(intNum + "는 소수입니다");
+	}else {
+	} 
+}
+}
